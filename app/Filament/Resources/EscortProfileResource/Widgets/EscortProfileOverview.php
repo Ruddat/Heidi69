cebbb977955853283b267cdbe534a507e9cad9cf
HP-LAPTOP\HP <ingo.ruddat@gmail.com>
2022-11-13 11:19:59 +0100

    EscortprofilOverview angelegt

---
+++ app/Filament/Resources/EscortProfileResource/Widgets/EscortProfileOverview.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\EscortProfileResource\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+
+class EscortProfileOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        return [
+            //
+        ];
+    }
+}
