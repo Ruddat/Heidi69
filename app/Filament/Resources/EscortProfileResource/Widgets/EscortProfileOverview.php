1f57afd7a859e1c32a0e5ec0ef75dd594066054c
HP-LAPTOP\HP <ingo.ruddat@gmail.com>
2022-11-13 13:39:51 +0100

    Card Widget eingerichtet

--- app/Filament/Resources/EscortProfileResource/Widgets/EscortProfileOverview.php
+++ app/Filament/Resources/EscortProfileResource/Widgets/EscortProfileOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\EscortProfileResource\Widgets;
 
+use App\Models\EscortProfile;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 
@@ -9,8 +10,16 @@ class EscortProfileOverview extends BaseWidget
 {
     protected function getCards(): array
     {
+
+       // $us = Country::where('country_code', 'us')->withCount('EscortProfile')->first();
+
         return [
-            //
+
+            Card::make('Alle Anzeigen', EscortProfile::all()->count()),
+            Card::make('Bounce rate', '21%'),
+         //   Card::make($us->name, 'Bounce rate', '21%'),
+            Card::make('Average time on page', '3:12'),
+
         ];
     }
 }
